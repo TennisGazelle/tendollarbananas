458fa2d3ad436626014cd1f0547c4fcb01e1601c
Daniel Lopez <daniel@simplerose.com>
2024-06-01 00:35:12 -0700

    init template

---
+++ src/app/views/dashboard/Commodity.jsx
@@ -0,0 +1,45 @@
+import { Fragment } from "react";
+import { Card, Grid, styled, useTheme } from "@mui/material";
+import RowCards from "./shared/RowCards";
+import StatCards from "./shared/StatCards";
+import Campaigns from "./shared/Campaigns";
+import StatCards2 from "./shared/StatCards2";
+import DoughnutChart from "./shared/Doughnut";
+import UpgradeCard from "./shared/UpgradeCard";
+import TopSellingTable from "./shared/TopSellingTable";
+
+// STYLED COMPONENTS
+const ContentBox = styled("div")(({ theme }) => ({
+  margin: "30px",
+  [theme.breakpoints.down("sm")]: { margin: "16px" }
+}));
+
+const Title = styled("span")(() => ({
+  fontSize: "1rem",
+  fontWeight: "500",
+  marginRight: ".5rem",
+  textTransform: "capitalize"
+}));
+
+const SubTitle = styled("span")(({ theme }) => ({
+  fontSize: "0.875rem",
+  color: theme.palette.text.secondary
+}));
+
+const H4 = styled("h4")(({ theme }) => ({
+  fontSize: "1rem",
+  fontWeight: "500",
+  marginBottom: "16px",
+  textTransform: "capitalize",
+  color: theme.palette.text.secondary
+}));
+
+export default function Commodity() {
+    return (
+        <Fragment>
+            <ContentBox className="commodity">
+            <H4>Wassup there?</H4>
+            </ContentBox>
+        </Fragment>
+    );
+}
